1badf7f094f426c8c4a9468e6f383278d66ad722
riccardocadoni <rcadoni.dev@gmail.com>
2024-01-20 01:39:35 +0100

    feat: added Footer component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Mulish } from "next/font/google";
+import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -6,10 +7,11 @@ const mulish = Mulish({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`flex min-h-screen flex-col items-center ${mulish.className}`}>
+    <div className={`flex min-h-screen flex-col items-center gap-10 pb-8 ${mulish.className}`}>
       <main className="flex flex-grow justify-center items-center">
         <Component {...pageProps} />
       </main>
+      <Footer />
     </div>
   );
 }
