5eeb521344ec94eee189c4868a29147824803dfa
riccardocadoni <rcadoni.dev@gmail.com>
2024-01-20 01:06:59 +0100

    feat: initialized component library and integrated Mulish font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import { Mulish } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const mulish = Mulish({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`flex min-h-screen flex-col items-center ${mulish.className}`}>
+      <main className="flex flex-grow justify-center items-center">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
